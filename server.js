93bef12a790567ff8113067e165117200a99e841
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-05-18 11:50:32 +0530

    setup project and database connection

---
+++ server.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import {connectDB} from './config/db.js';
+import { app } from './app.js';
+
+const PORT = process.env.PORT || 8000;
+
+
+// database connection
+connectDB();
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port:${PORT}`)
+})
+
+
+
